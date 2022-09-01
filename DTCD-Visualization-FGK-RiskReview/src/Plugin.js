7db6996f28f8ef797f9a433085f5d0b46147f785
Vitaliy Kalinovskiy <yb24.kalinovskiy@gmail.com>
2022-09-01 10:41:32 +0200

    added Clicked event

--- DTCD-Visualization-FGK-RiskReview/src/Plugin.js
+++ DTCD-Visualization-FGK-RiskReview/src/Plugin.js
@@ -38,7 +38,7 @@ export class VisualizationFgkRiskReview extends PanelPlugin {
     this.#guid = guid;
     this.#logSystem = new LogSystemAdapter('0.5.0', guid, pluginMeta.name);
     this.#eventSystem = new EventSystemAdapter('0.4.0', guid);
-    this.#eventSystem.registerPluginInstance(this);
+    this.#eventSystem.registerPluginInstance(this, ['Clicked']);
     this.#storageSystem = new StorageSystemAdapter('0.5.0');
     this.#dataSourceSystem = new DataSourceSystemAdapter('0.2.0');
 
@@ -51,6 +51,11 @@ export class VisualizationFgkRiskReview extends PanelPlugin {
     const view = new VueJS({
       data: () => ({}),
       render: h => h(PluginComponent),
+      methods: {
+        publishEventClicked: (value) => {
+          this.#eventSystem.publishEvent('Clicked', value);
+        },
+      }
     }).$mount(selector);
 
     this.#vueComponent = view.$children[0];
